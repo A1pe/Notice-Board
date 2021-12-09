8ef2c96f460e294b9fa4e9b260b139d2b433f279
Eddy <46862196+A1pe@users.noreply.github.com>
2021-12-09 17:39:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-let express = require('express')
 
-let app = express()
+const express = require('express')
+
+const app = express()
+
+
 
 app.listen(3000, function(){
     console.log("자 이제 시작이야! 내꿈을!")
 })
+
+app.get('/public/main.html', function (req, res) {
+    res.end()
+})
+
 
 app.get('/', function (req, res) {
     res.send("<h1>hi friends!<h1>")
